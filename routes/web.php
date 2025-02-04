caa725b4c4ee6ce4a12b011738471f5956a97cfd
iwindd <freewinendlife@gmail.com>
2025-02-04 19:08:21 +0700

    refactor routes to use controller methods for home and other pages

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,8 @@
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name("home");
-
-Route::get("home", [PageController::class, "home"])->name("home");
-Route::get("executives", [PageController::class, "executives"])->name("executives");
-Route::get("teachers", [PageController::class, "teachers"])->name("teachers");
-Route::get("students", [PageController::class, "students"])->name("students");
-Route::get("majors", [PageController::class, "majors"])->name("majors");
+Route::get("/", [PageController::class, "home"])->name("home");
+Route::get("/executives", [PageController::class, "executives"])->name("executives");
+Route::get("/teachers", [PageController::class, "teachers"])->name("teachers");
+Route::get("/students", [PageController::class, "students"])->name("students");
+Route::get("/majors", [PageController::class, "majors"])->name("majors");
