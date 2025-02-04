7065cd591e7760e2cad9c3f4cee9a7f63d448c51
iwindd <freewinendlife@gmail.com>
2025-02-04 19:21:11 +0700

    refactor navigation styles and extend Tailwind CSS configuration with custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -12,7 +13,41 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+                sarabun: ['Sarabun', ...defaultTheme.fontFamily.sans]
+            },
+
+            colors: {
+                dark: {
+                    'eval-0': '#151823',
+                    'eval-1': '#222738',
+                    'eval-2': '#2A2F42',
+                    'eval-3': '#2C3142',
+                },
+                'primary': {
+                    DEFAULT: colors.indigo[500],
+                    ...colors.indigo
+                },
+                'danger': {
+                    DEFAULT: colors.rose[500],
+                    ...colors.rose
+                },
+                'success': {
+                    DEFAULT: colors.emerald[500],
+                    ...colors.emerald
+                },
+                'warning': {
+                    DEFAULT: colors.yellow[500],
+                    ...colors.yellow
+                },
+                'secondary': {
+                    DEFAULT: colors.gray[500],
+                    ...colors.gray
+                },
+                'info': {
+                    DEFAULT: colors.sky[500],
+                    ...colors.sky
+                }
             },
         },
     },
