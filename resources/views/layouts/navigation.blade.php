ac94d34e2e6d1d11300fc785ba0d19a6b3b6587f
iwindd <freewinendlife@gmail.com>
2025-02-09 15:09:55 +0700

    add responsive navigation menu with toggle functionality for mobile view

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -28,7 +28,37 @@
                         {{ __('navbar.majors') }}
                     </x-nav-link>
                 </div>
+                <!-- Hamburger -->
+                <div class="-mr-2 flex items-center sm:hidden">
+                    <button @click="open = !open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition">
+                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <path :class="{'hidden': open, 'inline-flex': !open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            <path :class="{'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
             </div>
+        </div>
+    </div>
+
+    <!-- Responsive Navigation Menu -->
+    <div :class="{'block': open, 'hidden': !open}" class="sm:hidden transition-all duration-300 ease-out transform" x-show="open" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0 transform -translate-y-4" x-transition:enter-end="opacity-100 transform translate-y-0" x-transition:leave="transition ease-in duration-300" x-transition:leave-start="opacity-100 transform translate-y-0" x-transition:leave-end="opacity-0 transform -translate-y-4">
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                {{ __('navbar.home') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('executives')" :active="request()->routeIs('executives')">
+                {{ __('navbar.executives') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('teachers')" :active="request()->routeIs('teachers')">
+                {{ __('navbar.teachers') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('students')" :active="request()->routeIs('students')">
+                {{ __('navbar.students') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('majors')" :active="request()->routeIs('majors')">
+                {{ __('navbar.majors') }}
+            </x-responsive-nav-link>
         </div>
     </div>
 </nav>
