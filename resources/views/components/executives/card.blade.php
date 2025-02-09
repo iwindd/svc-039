025ee01ff77d4022d2e3b9815dc5026f1fe308b4
iwindd <freewinendlife@gmail.com>
2025-02-09 15:02:07 +0700

    update executive card component for improved layout and responsiveness

--- resources/views/components/executives/card.blade.php
+++ resources/views/components/executives/card.blade.php
@@ -9,13 +9,13 @@
 
 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 text-gray-900">
-        <header class="border-b pb-1 flex items-center space-x-2">
+        <header class="border-b pb-1 flex items-start sm:items-center flex-col sm:flex-row sm:space-x-2">
             <h2 class="font-bold text-xl">{{$name}}</h2>
-            <span class="text-xl">-</span>
-            <p class="text-lg">{{$position}}</p>
+            <span class="text-xl hidden sm:block">-</span>
+            <p class="text-sm">{{$position}}</p>
         </header>
         <article class="grid grid-cols-3 gap-2 mt-2">
-            <div class="col-span-2 flex items-center px-5">
+            <div class="col-span-2 flex items-center sm:px-5">
                 <ul class="space-y-2">
                     <li class="space-y-1">
                         <span class="font-bold bg-primary-100 p-1 px-2 rounded-sm">ประวัติการศึกษา</span>
